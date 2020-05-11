223c627788347a4ae6ddc9aaefc6072c7f3bdacf
MattBasil <65145719+MattBasil@users.noreply.github.com>
2020-05-10 22:15:03 -0500

    First push
    Welcome to the Git

---
+++ ITPgitHub/ITPgitHub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace ITPgitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine("Welcome to the Git!");
+
+        }
+    }
+}
